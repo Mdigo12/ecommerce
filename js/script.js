a7608c803f3bd7d48339222a4543271818456409
Mdigo12 <mdigojacob@gmail.com>
2020-02-24 18:19:49 +0300

    cart button to pick items on click by user

--- js/script.js
+++ js/script.js
@@ -7,5 +7,20 @@
   $(".search-icon").click(function(){
     $(".search-bar").toggle();
   });
+  $(".btn_add_cart").click(function(){
+    var newImage = $('<img src ="images/watch4.jpeg" alt="image" width="50px" height = "50px">');
+    $("#cart_image").prepend(newImage);
+    
+    $("#name").prepend("Ksh.1500");
+    $("#name").before("Wrist Watch");
+    $("#delete").show();
+    alert("One Item Added to Cart");
+    $("#delete").click(function(){
+      $(".list").remove();
+      $("#delete").hide();
+    });
+    
+
+});
   
 });
