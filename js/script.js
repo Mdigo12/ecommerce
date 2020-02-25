96fc75a5d76df6360734c4c01f0e0dc729b5aff3
Mdigo12 <mdigojacob@gmail.com>
2020-02-25 09:42:20 +0300

    final f

--- js/script.js
+++ js/script.js
@@ -1,46 +1,46 @@
 
 
-   $(document).ready(function(){
-  $(".view_cart").click(function(){
+$(document).ready(function () {
+  $(".view_cart").click(function () {
     $(".cart").show();
   });
-  $(".search-icon").click(function(){
+  $(".search-icon").click(function () {
     $(".search-bar").toggle();
   });
-  $(".btn_add_cart").click(function(){
+  $(".btn_add_cart").click(function () {
     var newImage = $('<img src ="images/watch4.jpeg" alt="image" width="50px" height = "50px">');
     $("#cart_image").prepend(newImage);
-    
+
     $("#name").prepend("Ksh.1500");
     $("#name").before("Wrist Watch");
     $("#delete").show();
     alert("One Item Added to Cart");
-    $("#delete").click(function(){
+    $("#delete").click(function () {
       $(".list").remove();
       $("#delete").hide();
 
-    //   var cartCount = 1;
-    //   var newCartCount;
-    //   var result;
-    //   function sum(){
-    //   newCartCount+=cartCount;
-    //   return newCartCount;
-    //   result = newCartCount.toString();
-    // }
-    //   $("#item_number").append(result);
-     });
-  //   $( "#item_list").click(function() {
-  //   $( "#item_number" ).append($( "<li>" ));
-  //   var n = $( "li" ).length;
-  //   $( "span" ).text(n);
-  // })
-  // // Trigger the click to start
-  // .trigger( "click" );
-  
-});
-var n =
-$("#item_list11")
-    
+      //   var cartCount = 1;
+      //   var newCartCount;
+      //   var result;
+      //   function sum(){
+      //   newCartCount+=cartCount;
+      //   return newCartCount;
+      //   result = newCartCount.toString();
+      // }
+      //   $("#item_number").append(result);
+    });
+    //   $( "#item_list").click(function() {
+    //   $( "#item_number" ).append($( "<li>" ));
+    //   var n = $( "li" ).length;
+    //   $( "span" ).text(n);
+    // })
+    // // Trigger the click to start
+    // .trigger( "click" );
 
-  
+  });
+  var n =
+    $("#item_list11")
+
+
+
 });
